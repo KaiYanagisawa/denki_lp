1f36bf3ea430063fe173290a99221d63cceb3555
KaiYanagisawa <12730ky@gmail.com>
2021-04-06 22:24:57 +0900

    edit campaign

--- js/jquery/jquery.js
+++ js/jquery/jquery.js
@@ -19,5 +19,18 @@ $(function() {
 
 //ナビゲーション
 $(function(){
-    
+    // ハンバーガーメニュークリックイベント
+    $('.header__menu').click(function(){
+      if($('.nav-sp').hasClass('open')){
+        // ナビゲーション非表示
+        $('.nav-sp').removeClass('open');
+        // ハンバーガーメニューを元に戻す
+        $(this).removeClass('open');
+      }else{
+        // ナビゲーションを表示
+        $('.nav-sp').addClass('open');
+        // ハンバーガーメニューを✖印に変更
+        $(this).addClass('open');
+      }
+    });
 });
