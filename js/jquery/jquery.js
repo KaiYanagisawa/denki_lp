4922701c0b4fc771a223ea8c34a05faf3d65e159
KaiYanagisawa <12730ky@gmail.com>
2021-04-11 17:24:09 +0900

    edit jquery

--- js/jquery/jquery.js
+++ js/jquery/jquery.js
@@ -1,64 +1,66 @@
 $(function() {
-   //トップへ戻るボタン
-    var pagetop = $('#page_top');   
+    //トップへ戻るボタン
+    var pagetop = $('#page_top');
     pagetop.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {  
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
             pagetop.fadeIn();
         } else {
             pagetop.fadeOut();
         }
     });
-    pagetop.click(function () {
+    pagetop.click(function() {
         $('body,html').animate({
             scrollTop: 0
-        }, 500); 
+        }, 500);
         return false;
     });
 
     //ナビゲーションスクロールpc
-    $('header a').click(function(){
+    $('header a').click(function() {
         var topHeight = $('header').height();
         var id = $(this).attr('href');
-        var position = ($(id).offset().top)-(topHeight);
+        var position = ($(id).offset().top) - (topHeight);
         $('body,html').animate({
             scrollTop: position
-        },500);
+        }, 500);
         return false;
     });
 
     // ナビゲーションスクロールsp
-    $('.header-menu a').click(function(){
+    $('.header-menu a').click(function() {
         var topHeight = $('header').height();
         var id = $(this).attr('href');
-        var position = ($(id).offset().top)-(topHeight);
+        var position = ($(id).offset().top) - (topHeight);
         $('body,html').animate({
             scrollTop: position
-        },500);
+        }, 500);
         //クリックしたら閉じる
         $('.header-menu').removeClass('open');
         return false;
     });
 
     // ハンバーガーメニュークリックイベント
-    $('.header__hamburger').click(function(){
-        if($('.header-menu').hasClass('open')){
-          // ナビゲーション非表示
-          $('.header-menu').removeClass('open');
-        }else{
-          // ナビゲーションを表示
-          $('.header-menu').addClass('open');
-        }
-      });  
-
-    //フッターsp広告
-    var pageFooter = $('.campaign-footer');   
-    pageFooter.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {  
-            pageFooter.fadeIn();
+    $('.header__hamburger').click(function() {
+        if ($('.header-menu').hasClass('open')) {
+            // ナビゲーション非表示
+            $('.header-menu').removeClass('open');
         } else {
-            pageFooter.fadeOut();
+            // ナビゲーションを表示
+            $('.header-menu').addClass('open');
         }
     });
+
+    //フッターsp広告
+    if (window.matchMedia('(max-width:760px)').matches) {
+        var pageFooter = $('.campaign-footer');
+        pageFooter.hide();
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                pageFooter.fadeIn();
+            } else {
+                pageFooter.fadeOut();
+            }
+        });
+    }
 });
